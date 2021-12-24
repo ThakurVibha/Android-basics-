e1b0b1cd0da02ffb9faae30b7aee695bb8ce845e
ThakurVibha <vibhathakur39@gmail.com>
2021-12-24 00:08:14 -0800

    retrofit logging

--- app/src/main/java/com/example/crypton/kotlinbasics/retrofitbasics/TaskService.kt
+++ app/src/main/java/com/example/crypton/kotlinbasics/retrofitbasics/TaskService.kt
@@ -1,5 +1,10 @@
 package com.example.crypton.kotlinbasics.retrofitbasics
 
+import android.os.Build
+import android.util.Log
+import com.example.crypton.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -9,7 +14,7 @@ import retrofit2.http.POST
 import retrofit2.http.Path
 
 var BASE_URL = "https://api.github.com/"
-var BASE_URL_GITHUB=""
+var BASE_URL_GITHUB = ""
 
 interface TaskService {
 
@@ -17,7 +22,7 @@ interface TaskService {
     fun getRepos(@Path("user") user: String): Call<List<GithubRepoModel>>
 
     @POST("posts")
-    fun createAccount(@Body user:User):Call<User>
+    fun createAccount(@Body user: User): Call<User>
 
 }
 
@@ -26,8 +31,20 @@ object RetrofitInstance {
     var service: TaskService
 
     init {
+        //create okhttp client
+        val okHttpClient = OkHttpClient.Builder()
+        //create interceptor
+        var httpLoggingInterceptor = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+            //add to okhttpClient
+            okHttpClient.addInterceptor(httpLoggingInterceptor)
+        }
+
+
         var builder =
-            Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/").addConverterFactory(GsonConverterFactory.create())
+            Retrofit.Builder().baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create()).client(okHttpClient.build())
                 .build()
 
         service = builder.create(TaskService::class.java)
